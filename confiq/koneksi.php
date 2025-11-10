2808a80a7fe46c423983b06bf90a83963c69aa72
veraarmyanti445 <veraarmyanti445@gmail.com>
2025-11-10 10:11:19 +0800

    praktikum 1

---
+++ confiq/koneksi.php
@@ -0,0 +1,19 @@
+<?php
+
+$servername = "localhost";
+$database = "uinsi_2441919017";
+$username = "root";
+$password = "";
+
+//Buat Koneksi Database
+$conn = mysqli_connect($servername,$username,$password,$database);
+
+//Cek Koneksi
+
+if (!$conn) {
+    die("Koneksi Gagal".mysqli_connect_eror());
+}
+
+echo "Koneksi Berhasil!";
+// mysqli_close();
+?>
